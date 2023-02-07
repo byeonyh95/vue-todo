28667eb12fac1cdd4617433658f2e5c506a43936
byeonyh95 <byeonyh95@gmail.com>
2023-02-07 16:03:46 +0900

    컴포넌트 만들기

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      newTodoItem: "",
+    }
+  },
+  methods: {
+    addTodo: function(){
+      console.log(this.newTodoItem);
+      //저장하는 로직
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
