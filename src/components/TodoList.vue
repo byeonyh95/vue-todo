8d4ebb80df7150e549c2fdbcc6db5b79017f625b
byeonyh95 <byeonyh95@gmail.com>
2023-02-07 23:53:10 +0900

    TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,83 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fa-sharp fa-solid fa-square-check" v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fa-regular fa-square-minus"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+ data: function(){
+  return{
+    todoItems: [],
+  }
+ },
 
+ methods:{
+   removeTodo: function (todoItem, index) {
+    localStorage.removeItem(todoItem);
+    this.todoItems.splice(index,1);
+   },
+
+   toggleComplete: function (todoItem, index){
+    console.log(index);
+     if (todoItem.completed){
+       todoItem.completed = false;
+     }else{
+       todoItem.completed = true;
+     }
+   }
+ },
+
+  created: function(){
+    if (localStorage.length > 0){
+      for (var i = 0; i < localStorage.length; i++){
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
